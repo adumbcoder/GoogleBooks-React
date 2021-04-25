725a734dc11a3e2faff05fd447b00a2ab1a986a6
adumbcoder <70477253+adumbcoder@users.noreply.github.com>
2021-04-24 23:41:08 -0400

    scaffold, search and save pages done, all npms

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css"
+
+function Header() {
+    return(
+        <div>
+            <h1 className="Header">(React) Google Books Search</h1>
+            <p>Search for and Save Books of Interest</p>
+        </div>
+    ) 
+  }
+
+export default Header;
---
+++ src/components/List/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./style.css";
+
+// This file exports both the List and ListItem components
+
+
+export function SavedList ({ children}) {
+  return (
+    <div className="list-overflow-container">
+      <ul className="list-group">{children}</ul>
+    </div>
+  )
+}
+
+export function SearchList({ children }) {
+  return (
+    <div className="list-overflow-container">
+      <ul className="list-group">{children}</ul>
+    </div>
+  );
+}
+
+export function ListItem({ children }) {
+  return <li className="list-group-item">{children}</li>;
+}
